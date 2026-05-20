17d9cdf5920038c6274fcc14f52d13cc7254cdec
7absi92-dot <7absi92@gmail.com>
2026-05-20 22:41:20 +0400

    app.py

--- app.py
+++ app.py
@@ -919,13 +919,6 @@ submission = {
     "hour",
     "weekend",
     "month",
-    "hour_sin",
-    "hour_cos",
-    "month_sin",
-    "month_cos",
-    "lag_difference_24_1"
-],
-"advanced_features_used": advanced_features_used if "advanced_features_used" in globals() else [],
         "y_target_shifted_by_horizon": True
     },
     "modeling_evidence": {
